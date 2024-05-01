bc034f946c7479b4fa7e5c64d64a2b7657e89842
labattaria <107855108+labattaria@users.noreply.github.com>
2024-05-01 20:21:38 +0300

    counter element are moved to another component

--- src/components/Menu/MenuItem.jsx
+++ src/components/Menu/MenuItem.jsx
@@ -1,16 +1,30 @@
 import './menu/menu.css'
 import Button from '../Button/Button';
-import MenuOrderCounter from '../Menu/MenuOrderCounter'
+// import MenuOrderCounter from '../Menu/MenuOrderCounter'
 import { useState } from 'react';
 
 const MenuItem = ({ pizza }) => {
     const { id, name, unitPrice, imageUrl, ingredients, soldOut } = pizza;
 
     const [isToggled, setIsToggled] = useState(false);
+    const [count, setCount] = useState(0);
 
     const handleButtonDescrChange = () => {
         setIsToggled(!isToggled);
     };
+
+    const handleIncrementClick = () => {
+        setCount(prevCount => prevCount + 1);
+    }
+
+    const handleDecrementClick = () => {
+        if (count === 0) {
+            alert("You cannot order 0 or less pizzas 😄");
+            return;
+        }
+
+        setCount(prevCount => prevCount - 1);
+    }
 
     return (
         <li className="pizza" key={id}>
@@ -20,7 +34,12 @@ const MenuItem = ({ pizza }) => {
                 <p className="pizza__ingredients">{ingredients.join(', ')}</p>
                 <div className="pizza__actions">
                     {!soldOut ? <p className="pizza__price">€{unitPrice}</p> : <p className="pizza__price">Sold out</p>}
-                    {isToggled && <MenuOrderCounter className="button" />}
+                    {isToggled &&
+                        <div className="counter">
+                            <Button className="className" label="-" onClick={handleDecrementClick} />
+                            <p>{count}</p>
+                            <Button className="className" label="+" onClick={handleIncrementClick} />
+                        </div>}
                     {!soldOut && <Button className="button" onClick={handleButtonDescrChange} label={isToggled ? "Delete" : "Add to cart"} />}
                 </div>
             </div>
