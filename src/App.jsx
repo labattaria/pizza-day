442d8ff2250c13776f1b8f1f306bca5cbc11938e
labattaria <107855108+labattaria@users.noreply.github.com>
2024-05-08 02:20:31 +0300

    added react router and declared page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
 // import './App.css'
-import Header from './components/Header/Header'
-import Heading from './components/Heading/Heading'
-import SubHeading from './components/SubHeading/SubHeading'
-import LoginForm from './components/LoginForm/LoginForm'
-import Menu from './components/Menu/Menu'
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Login from './pages/Login';
+import Menu from './pages/Menu';
 
 function App() {
 
   return (
     <div className="wrapper">
       <Header className="header" />
-      <main className="content">
-        <Heading className="title" label="The best pizza." />
-        <SubHeading className="sub-title" label="👋 Welcome! Please start by telling us your name:" />
-        <LoginForm className="login-form" />
-      </main>
 
-      <Menu />
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/menu' element={<Menu />} />
+      </Routes>
     </div>
   );
 }
