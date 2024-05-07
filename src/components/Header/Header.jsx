551f2fd91940ffcee4326452cd169b019da9f08b
labattaria <107855108+labattaria@users.noreply.github.com>
2024-05-08 02:25:13 +0300

    implemented pages navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import Input from '../Input/Input';
+import { NavLink } from 'react-router-dom';
 
 const Form = ({ className, placeholder }) => {
 
@@ -14,6 +15,18 @@ const Header = ({ className }) => {
     return (
         <header className={className}>
             <a className="logo" href="/">Pizza Day</a>
+
+            <nav>
+                <ul className="header__nav">
+                    <li className="item">
+                        <NavLink to='/' className="link">Login</NavLink>
+                    </li>
+                    <li className="item">
+                        <NavLink to='/menu' className="link">Menu</NavLink>
+                    </li>
+                </ul>
+            </nav>
+
             <Form placeholder="Search for the order #" />
         </header>
     );
