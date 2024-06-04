4a8cb5aa5960aba709c3105fa8b14d1efe81c61f
labattaria <107855108+labattaria@users.noreply.github.com>
2024-06-04 13:24:54 +0300

    implemented new routes and pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Login from './pages/Login';
 import Menu from './pages/Menu';
+import Cart from './pages/Cart';
 import Order from './pages/Order';
+import DinamicUserOrder from './pages/DinamicUserOrder';
 import PageNotFound from './pages/PageNotFound';
 
 function App() {
@@ -15,7 +17,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Login />} />
         <Route path='/menu' element={<Menu />} />
+        <Route path='/cart' element={<Cart />} />
         <Route path='/order/new' element={<Order />} />
+        <Route path='/order/:id' element={<DinamicUserOrder />} />
 
         <Route path='*' element={<PageNotFound />} />
       </Routes>
