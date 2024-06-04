51e9aff4997df1342e343bff174a9afb455b453c
labattaria <107855108+labattaria@users.noreply.github.com>
2024-06-04 13:31:34 +0300

    implemented redux toolkit store, slice and provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import LoginContext from './context/LoginContext.jsx';
+import { Provider } from 'react-redux'
+import { store } from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
         <LoginContext>
-            <App />
+            <Provider store={store}>
+                <App />
+            </Provider>
         </LoginContext>
     </BrowserRouter>);
