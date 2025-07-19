a995863317581fb79e2d84712f8827ed17684764
labattaria <107855108+labattaria@users.noreply.github.com>
2025-07-19 21:08:13 +0300

    refactor styles for the Cart component using SCSS module

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,17 +1,18 @@
 import { useSelector } from "react-redux"
 import { Link } from "react-router-dom";
+import { sticky__footer, footer__items, footer__item, item__price, item__link } from '../Cart/cart.module.scss';
 
 const StickyFooter = () => {
     const { totalItems, totalPrice } = useSelector(store => store.cart);
 
     return (
-        <div className="sticky__footer">
-            <p className="footer__items">
-                <span className="footer__item">{totalItems} {totalItems > 1 ? "pizzas" : "pizza"}</span>
-                <span className="footer__item footer__item--price">€{totalPrice}</span>
+        <div className={sticky__footer}>
+            <p className={footer__items}>
+                <span className={footer__item}>{totalItems} {totalItems > 1 ? "pizzas" : "pizza"}</span>
+                <span className={`${footer__item} ${item__price}`}>€{totalPrice}</span>
             </p>
 
-            <Link to="/cart" className="footer__item footer__item--link">OPEN CART</Link>
+            <Link to="/cart" className={`${footer__item} ${item__link}`}>OPEN CART</Link>
         </div>
     );
 };
