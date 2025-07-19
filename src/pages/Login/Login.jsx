5a83b07e49e9180b1cbbdf5856d129a879d098c6
labattaria <107855108+labattaria@users.noreply.github.com>
2025-07-19 21:05:48 +0300

    refactor styles for the Login page component using SCSS module

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,11 @@
 import Heading from '../../components/Heading/Heading';
 import SubHeading from '../../components/SubHeading/SubHeading';
 import LoginForm from '../../components/LoginForm/LoginForm';
-import { title, sub__title, login__form } from '../../pages/Login/login.module.scss';
+import { title, sub__title, login__form, content } from '../../pages/Login/login.module.scss';
 
 const Login = () => {
     return (
-        <main className="content">
+        <main className={content}>
             <Heading className={title} label="The best pizza." />
             <SubHeading className={sub__title} label="👋 Welcome! Please start by telling us your name:" />
             <LoginForm className={login__form} />
