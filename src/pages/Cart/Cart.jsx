244d1eaf6ff0b39ff892b8499c288b550080715c
labattaria <107855108+labattaria@users.noreply.github.com>
2025-07-19 19:04:05 +0300

    update cart page styles

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -7,7 +7,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { decrement, deleteFromCart, increment } from "../../redux/slices/cartSlice";
 import styles from '../Cart/cart.module.scss';
 
-const { cart, cart__title, cart__list, cart__product, cart__navigation, cart__empty, menu__link, order__link } = styles;
+const { cart, cart__title, cart__list, cart__product, cart__navigation, cart__empty, menu__link, order__link, counter, counter__items, counter__button } = styles;
 
 const Cart = () => {
     const { username } = useContext(LoginContext);
@@ -43,12 +43,12 @@ const Cart = () => {
                             return (
                                 <li className={cart__product} key={item.id}>
                                     <p>{item.quantity}× {item.name}</p>
-                                    <div className="counter">
+                                    <div className={counter}>
                                         <p>€{item.unitPrice * item.quantity}</p>
-                                        <div className="counter__items">
-                                            <Button className="counter__button" label="-" onClick={() => handleDecrementQuantity(item)} />
+                                        <div className={counter__items}>
+                                            <Button className={counter__button} label="-" onClick={() => handleDecrementQuantity(item)} />
                                             <p>{item.quantity}</p>
-                                            <Button className="counter__button" label="+" onClick={() => handleIncrementQuantity(item)} />
+                                            <Button className={counter__button} label="+" onClick={() => handleIncrementQuantity(item)} />
                                         </div>
 
                                         <Button className="button" onClick={() => handleDeleteFromCart(item)} label="Delete" />
