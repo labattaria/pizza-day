d5b5d911f462e557c1f60408825e8a044cbf8b13
labattaria <107855108+labattaria@users.noreply.github.com>
2025-07-19 20:48:06 +0300

    refactor styles for the Heading component using SCSS module

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,8 +1,10 @@
+import { text__yellow } from '../Heading/heading.module.scss';
+
 const Heading = ({ className, label }) => {
 
     return (
         <h1 className={className}>{label}<br />
-            <span className="text-yellow">Straight out of the oven, straight to you.</span>
+            <span className={text__yellow}>Straight out of the oven, straight to you.</span>
         </h1>
     );
 };
